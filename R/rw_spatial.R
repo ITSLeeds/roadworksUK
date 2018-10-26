b7f06ccc1952ae4cf3bed148b6c4167d97879359
Robin Lovelace <rob00x@gmail.com>
2018-10-26 14:38:39 +0100

    Remove placeholder function name

--- R/rw_spatial.R
+++
@@ -1,9 +0,0 @@
-#' Convert an data frame containing roadworks data into a spatial data object
-#'
-#' This function will take a data frame and make it spatial
-#'
-#' @param x param
-#' @export
-rw_spatial = function(x) {
-  print(paste0("hello", x))
-}
